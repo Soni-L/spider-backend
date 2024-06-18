57945a85870bbe9bcdf7a955346bcd09ff9db50d
soni <acdsoni@gmail.com>
2024-06-18 15:47:10 +0200

    cors and puppeter

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import express from "express";
+import cors from "cors";
+import puppeteer from "puppeteer";
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  console.log(req.query.url);
+  res.json({ hello: "hello world" });
+  //   const browser = await puppeteer.launch();
+  //   const page = await browser.newPage();
+  //   await page.goto("https://example.org/", { waitUntil: "networkidle0" });
+  //   const data = await page.evaluate(() => document.querySelector("*").outerHTML);
+
+  //   await browser.close();
+  //   res.json(data);
 });
 
 app.listen(port, () => {
