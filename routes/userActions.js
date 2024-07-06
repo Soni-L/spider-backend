ff11fd520cca007959f60b7368642b29ffc1d432
soni <acdsoni@gmail.com>
2024-07-07 01:32:42 +0200

    page eval

--- routes/userActions.js
+++ routes/userActions.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { getPageById } from "../puppeteerManager.js";
+import { getPageById, createNewPage } from "../puppeteerManager.js";
+import { v4 as uuidv4 } from "uuid";
 const router = Router();
 
 async function findElementOrAncestor(page, xpath) {
@@ -31,14 +32,31 @@ async function findElementOrAncestor(page, xpath) {
 router.get("/", async (req, res) => {
   try {
     const id = req.cookies.client_tab_session_id;
-    // const url = decodeURIComponent(req.query.url);
+    const url = decodeURIComponent(req.query.url);
     const page = getPageById(id);
+
+    if (!page) {
+      const newId = uuidv4();
+      await createNewPage(newId, browser);
+      const page = getPageById(id);
+      await page.goto(url, { waitUntil: "networkidle2", timeout: 100000 });
+      res.cookie("client_tab_session_id", id, {
+        httpOnly: true, // The cookie only accessible by the web server
+        secure: true, // Ensure the browser only sends the cookie over HTTPS
+        maxAge: 1000 * 60 * 15, // Cookie expiry time in milliseconds (15 minutes)
+      });
+
+      res.json({ html, styles: stylesheets, reload: true });
+    }
+
     let xpath = req.query.xpath;
 
     xpath = await findElementOrAncestor(page, xpath);
+    await page.waitForSelector(`::-p-xpath(${xpath})`);
     await Promise.all([
       page.waitForNavigation({ waitUntil: "networkidle0" }), // wait for navigation to complete
-      page.click(`::-p-xpath(${xpath})`), // replace with your element's selector
+      page.$eval(`::-p-xpath(${xpath})`, (element) => element.click()),
+      // page.click(`::-p-xpath(${xpath})`), // replace with your element's selector
     ]);
 
     const html = await page.content();
@@ -59,7 +77,7 @@ router.get("/", async (req, res) => {
         .join("\n");
     });
 
-    res.json({ html, styles: stylesheets, xpath });
+    res.json({ html, styles: stylesheets, xpath, reload: true });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
