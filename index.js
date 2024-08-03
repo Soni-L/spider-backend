4dcc224bb30feeb0b914120829b8daa492e3e710
soni <acdsoni@gmail.com>
2024-08-03 15:45:11 +0200

    socket.io

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { createServer } from "http";
 import { getBrowser } from "./backend_browser/puppeteerManager.js";
 import pageActions from "./routes/pageActions.js";
 import userActions from "./routes/userActions.js";
+import { Server } from "socket.io";
 
 const PORT = 5000;
+const SOCKET_PORT = 3000;
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: "*",
+    allowedHeaders: "*",
+  },
+});
+
+server.listen(SOCKET_PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("message", msg); // Broadcast the message to all clients
+  });
+});
+
 app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173", // Your React app's origin
     credentials: true, // Allow credentials (cookies) to be included
+    methods: "*",
+    allowedHeaders: "*",
   })
 );
 app.use(express.json());
